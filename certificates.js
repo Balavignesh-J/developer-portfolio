206e6e5be1d56194cd5143b969e43cb83c525543
Balavignesh J <balav3230@gmail.com>
2026-02-17 22:32:38 +0530

    Changed dynakic certificate loading

---
+++ certificates.js
@@ -0,0 +1,182 @@
+const certificatesData = {
+  nptel: [
+    {
+      title: "The Joy of Computing using Python",
+      image: "/img/My_Certificates/NPTEL/The_Joy_of_Computing_using_Python.jpg",
+      link: "https://drive.google.com/file/d/1Ar3x9gQ6UoUwBNWafCtLi8skUQEEnxVZ/view",
+    },
+    {
+      title: "Programming, Data Structures and Algorithms using Python",
+      image:
+        "/img/My_Certificates/NPTEL/Programming,_Data_Structures_and_Algorithms_using_Python.jpg",
+      link: "https://drive.google.com/file/d/1FmytVkvCx9eDxci-vlt3mJQhVEQOi2Be/view",
+    },
+    {
+      title: "Programming in Java",
+      image: "/img/My_Certificates/NPTEL/Programming In Java.jpg",
+      link: "https://drive.google.com/file/d/1B7-itPpBZW5GKcXL7y4Pr-F0uIkv9BjY/view",
+    },
+  ],
+  linkedinLearning: [
+    {
+      title: "Creating a Responsive Web Design:Advanced Techniques",
+      image:
+        "/img/My_Certificates/LinkedIn_Learning/Creating_a_Responsive_Web_Design_Advanced_Techniques.jpg",
+      link: "https://drive.google.com/file/d/1MzreukNJ8dS2yBfd43enbjU1OjG3wgQL/view",
+    },
+    {
+      title: "CSS: Advanced Layouts with Grid",
+      image:
+        "/img/My_Certificates/LinkedIn_Learning/CSS_Advanced_Layouts_with_Grid.jpg",
+      link: "https://drive.google.com/file/d/1MpRl7gAFytsiHclaQOzT1YMMzTNSjulY/view",
+    },
+    {
+      title: "CSS Essential Training",
+      image: "/img/My_Certificates/LinkedIn_Learning/CSS_Essential_Training.jpg",
+      link: "https://drive.google.com/file/d/12w29gIawCiqL_M9Y5WcKUpY3jYaXUQwa/view",
+    },
+    {
+      title: "HTML Essential Training (2020)",
+      image:
+        "/img/My_Certificates/LinkedIn_Learning/HTML_Essential_Training_(2020).jpg",
+      link: "https://drive.google.com/file/d/1IYBq2fEk8pPrSpna-1jUNYVjj_78koAE/view",
+    },
+    {
+      title: "HTML for Programmers",
+      image: "/img/My_Certificates/LinkedIn_Learning/HTML_for_Programmers.jpg",
+      link: "https://drive.google.com/file/d/16xxcYY0x1b4M6gfe8eWyixTlqbPk9O6e/view",
+    },
+    {
+      title: "Introduction to Web Design and Development",
+      image:
+        "/img/My_Certificates/LinkedIn_Learning/Introduction_to_Web_Design_and_Development.jpg",
+      link: "https://drive.google.com/file/d/1G96QMUo42og1YUxTiuUz3uyAz_9U4biP/view",
+    },
+  ],
+  naanMudhalvan: [
+    {
+      title: "EBPL",
+      image: "/img/My_Certificates/Naan_Mudhalvan/NM_Certificate.jpg",
+      link: "https://drive.google.com/file/d/1XeNYEdeAKT5yV69EfW-FeaSFz7EunLMd/view",
+    },
+    {
+      title: "Oracle Cloud Infrastructure 2025 Certified Al Foundations Associate",
+      image: "/img/My_Certificates/Naan_Mudhalvan/oracle1.jpg",
+      link: "https://drive.google.com/file/d/1TuHeEMuq15CpkZbB-bBfdBwwsUPlK9Hv/view",
+    },
+    {
+      title: "Oracle Cloud Infrastructure 2025 Certified Foundations Associate",
+      image: "/img/My_Certificates/Naan_Mudhalvan/oracle2.jpg",
+      link: "https://drive.google.com/file/d/1-qZNYl9NVJ6EN2S2Kt0lJsXlQv4NPkSU/view",
+    },
+    {
+      title: "UIUX",
+      image: "/img/My_Certificates/Naan_Mudhalvan/UIUX.jpg",
+      link: "https://drive.google.com/file/d/1tMt16_DuFhwpmMEvB9dQZ6dSAITi2PmG/view",
+    },
+    {
+      title: "UIUX HCL",
+      image: "/img/My_Certificates/Naan_Mudhalvan/HCL_GUVI.png",
+      link: "https://drive.google.com/file/d/13Fjdsg5uo5syf1Gz-6FLkK7EH7QG4z7p/view",
+    },
+  ],
+  infosysSpringboard: [
+    {
+      title: "Learning Java 9 - Object Oriented Programming",
+      image: "/img/My_Certificates/SpringBoard/Java.jpg",
+      link: "https://drive.google.com/file/d/1cCuwgbVUgTULboFsmJcsIWLAGH84Czmm/view",
+    },
+    {
+      title: "Basics of Python",
+      image: "/img/My_Certificates/SpringBoard/python.jpg",
+      link: "https://drive.google.com/file/d/1w2L4rDxKig530I7BEQ0btcubRDyq097c/view",
+    },
+  ],
+  symposiums: [
+    {
+      title: "C. Abdul Hakeem College of Engineering & Technology",
+      image: "./img/My_Certificates/Symposiums/Hakeem.jpeg",
+      link: "https://drive.google.com/file/d/1c5m92nm270MLMz05I-R33vCqyYW1jXx-/view",
+    },
+    {
+      title: "Ganadipathy Tulsi's Jain Engineering College Vellore",
+      image: "./img/My_Certificates/Symposiums/GTEC.jpg",
+      link: "https://drive.google.com/file/d/1sAcsmwggSYX-Psjl2-1wH6kmdWu1BE0Q/view",
+    },
+    {
+      title: "Annai Mira College of Engineering and Technology Vellore",
+      image: "./img/My_Certificates/Symposiums/AMCET.jpg",
+      link: "https://drive.google.com/file/d/15xi03Ow68DP-LjyKA4v0X28tlxXw0C3R/view",
+    },
+    {
+      title: "Gojan School of Business and Technology Chennai",
+      image: "./img/My_Certificates/Symposiums/Gojan.jpg",
+      link: "https://drive.google.com/file/d/1n0HhQ9u4Ykdu302oOOYzcqOgBA7qJByf/view",
+    },
+    {
+      title: "Global Institute Of Engineering And Technology",
+      image: "./img/My_Certificates/Symposiums/Global.jpg",
+      link: "https://drive.google.com/file/d/1XQuFxekXqvkorccEq0ahJSH0E8xrZe24/view",
+    },
+  ],
+  udemy: [
+    {
+      title: "Python Complete Course For Python Beginners",
+      image: "./img/My_Certificates/Udemy/Udemy_python.jpg",
+      link: "https://drive.google.com/file/d/12kZdj0rz-cq6LFlIuZ5-smV0RxgnBQUU/view",
+    },
+    {
+      title: "MS Word - Microsoft Word Course Beginner to Expert",
+      image: "./img/My_Certificates/Udemy/Udemy_msword.jpg",
+      link: "https://drive.google.com/file/d/1LLk36EcLOSs6o4ud5OzLKhp8ug0370cT/view",
+    },
+  ],
+};
+
+function renderCertificates(sectionId, certificates) {
+  const grid = document.querySelector(`#${sectionId} .projects-grid`);
+  if (!grid || !Array.isArray(certificates)) return;
+
+  certificates.forEach((cert) => {
+    const anchor = document.createElement("a");
+    anchor.href = cert.link;
+    anchor.target = "_blank";
+    anchor.rel = "noopener noreferrer";
+
+    const card = document.createElement("div");
+    card.className = "project-card";
+    card.style.width = "400px";
+    card.style.height = "250px";
+
+    const imageWrapper = document.createElement("div");
+    imageWrapper.className = "project-image";
+
+    const img = document.createElement("img");
+    img.className = "project-img";
+    img.src = cert.image;
+    img.alt = cert.title || "certificate-image";
+
+    const content = document.createElement("div");
+    content.className = "project-content";
+
+    const title = document.createElement("h3");
+    title.textContent = cert.title;
+
+    content.appendChild(title);
+    imageWrapper.appendChild(img);
+    card.appendChild(imageWrapper);
+    card.appendChild(content);
+    anchor.appendChild(card);
+    grid.appendChild(anchor);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  renderCertificates("nptel", certificatesData.nptel);
+  renderCertificates("linkedin-learning", certificatesData.linkedinLearning);
+  renderCertificates("naan-mudhalvan", certificatesData.naanMudhalvan);
+  renderCertificates("infosys-springboard", certificatesData.infosysSpringboard);
+  renderCertificates("Symposiums", certificatesData.symposiums);
+  renderCertificates("Udemy", certificatesData.udemy);
+});
+
